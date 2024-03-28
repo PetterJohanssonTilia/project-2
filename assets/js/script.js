03194acb20d8f531df26e0046050b927a1ba3c06
dolmerfresh <146840972+dolmerfresh@users.noreply.github.com>
2024-03-28 09:32:52 +0100

    script: Added gameobjects array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,39 @@
+// ======== Game objects ======== //
+
+const animals = [
+    { name: "Elephant", weight: 5000, size: "big" },
+    { name: "Giraffe", weight: 1800, size: "big" },
+    { name: "Lion", weight: 190, size: "small" },
+    { name: "Tiger", weight: 250, size: "small" },
+    { name: "Gorilla", weight: 180, size: "small" }
+];
+
+const foods = [
+    { name: "Apple", weight: 0.15, size: "tiny" },
+    { name: "Chicken Breast", weight: 0.2, size: "tiny" },
+    { name: "Broccoli", weight: 0.1, size: "tiny" },
+    { name: "Salmon Fillet", weight: 0.18, size: "tiny" },
+    { name: "Pizza", weight: 0.3, size: "tiny" }
+];
+
+const culturalObjects = [
+    { name: "Eiffel Tower", weight: 7300, size: "huge" },
+    { name: "Mona Lisa", weight: 0.8, size: "tiny" },
+    { name: "Great Wall of China", weight: 70000000, size: "huge" },
+    { name: "Rosetta Stone", weight: 0.762, size: "tiny" },
+    { name: "Taj Mahal", weight: 16340000, size: "huge" }
+];
+
+const cars = [
+    { name: "Fiat 500", weight: 900, size: "big" },
+    { name: "Hummer H1", weight: 3900, size: "big" },
+    { name: "Smart Fortwo", weight: 750, size: "big" },
+    { name: "Ford F-150", weight: 2300, size: "big" },
+    { name: "Bugatti Chiron", weight: 1995, size: "big" }
+];
+
+const categories = [animals, foods, culturalObjects, cars];
+
 /**
  * Run game after DOM is loaded
  * Add Event listeners for clicking guess-boxes
