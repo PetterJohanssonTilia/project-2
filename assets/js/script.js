dca17b2ff1cf1aa35ccd6d6c38bc1efa25472375
dolmerfresh <146840972+dolmerfresh@users.noreply.github.com>
2024-03-28 09:13:50 +0100

    script: Added DOM and Click event listeners

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Run game after DOM is loaded
+ * Add Event listeners for clicking guess-boxes
+ */
+document.addEventListener("DOMcontentLoaded", function(){
+document.getElementById("operand1").addEventListener("click", function(){}) 
+document.getElementById("operand2").addEventListener("click", function(){})    
+})
+
+
